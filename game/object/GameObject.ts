a29f2f9cba6880a8876ce2268e47e9da7d464c26
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-01-10 14:03:53 +0100

    base game object for the actual racing game

---
+++ game/object/GameObject.ts
@@ -0,0 +1,4 @@
+import CanvasRenderer from '../../engine/CanvasRenderer'
+import TickUpdatedGameObject from '../../engine/TickUpdatedGameObject.js'
+
+export default class GameObject extends TickUpdatedGameObject<CanvasRenderer> {}
