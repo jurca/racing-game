9baa47a3c9aac122dfdacb8c18439ea5a46be22e
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-07-31 16:08:58 +0200

    Make game object API more convenient to use in the actual game

--- game/object/GameObject.ts
+++ game/object/GameObject.ts
@@ -1,4 +1,47 @@
 import CanvasRenderer from '../../engine/CanvasRenderer'
+import Game from '../../engine/Game.js'
 import TickUpdatedGameObject from '../../engine/TickUpdatedGameObject.js'
+import Pseudo3DCamera from '../Pseudo3DCamera.js'
 
-export default class GameObject extends TickUpdatedGameObject<CanvasRenderer> {}
+interface IImplementation {
+  onRun?(): void
+  update?(game: Game<Pseudo3DCamera, CanvasRenderer<Pseudo3DCamera>>): void
+  onStop?(): void
+  render?(renderer: CanvasRenderer<Pseudo3DCamera>, deltaTime: number): void
+}
+
+export default class GameObject extends TickUpdatedGameObject<Pseudo3DCamera, CanvasRenderer<Pseudo3DCamera>> {
+  constructor(
+    private readonly implementation: IImplementation = {},
+  ) {
+    super()
+  }
+
+  public onRun(): void {
+    if (this.implementation.onRun) {
+      this.implementation.onRun()
+    }
+    super.onRun()
+  }
+
+  public updateTick(game: Game<Pseudo3DCamera, CanvasRenderer<Pseudo3DCamera>>): void {
+    if (this.implementation.update) {
+      this.implementation.update(game)
+    }
+    super.updateTick(game)
+  }
+
+  public onStop(): void {
+    super.onStop()
+    if (this.implementation.onStop) {
+      this.implementation.onStop()
+    }
+  }
+
+  public render(renderer: CanvasRenderer<Pseudo3DCamera>, deltaTime: number) {
+    if (this.implementation.render) {
+      this.implementation.render(renderer, deltaTime)
+    }
+    super.render(renderer, deltaTime)
+  }
+}
