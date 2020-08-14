ada21f8e313f3edfac3335c00057950892c583dc
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-08-14 14:55:57 +0200

    Fix outdated references

--- game/object/GameObject.ts
+++ game/object/GameObject.ts
@@ -1,11 +1,11 @@
-import CanvasRenderer from '../../engine/Canvas2DRenderer'
+import Canvas2DRenderer from '../../engine/Canvas2DRenderer'
 import Game from '../../engine/Game.js'
 import TickUpdatedGameObject from '../../engine/TickUpdatedGameObject.js'
 import Pseudo3DCamera from '../Pseudo3DCamera.js'
 
-export default class GameObject extends TickUpdatedGameObject<Pseudo3DCamera, CanvasRenderer<Pseudo3DCamera>> {
+export default class GameObject extends TickUpdatedGameObject<Pseudo3DCamera, Canvas2DRenderer<Pseudo3DCamera>> {
   constructor(
-    public readonly game: Game<Pseudo3DCamera, CanvasRenderer<Pseudo3DCamera>>,
+    public readonly game: Game<Pseudo3DCamera, Canvas2DRenderer<Pseudo3DCamera>>,
     x: number = 0, y: number = 0, z: number = 0,
   ) {
     super(x, y, z)
