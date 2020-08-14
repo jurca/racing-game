024bee5fc632bf6eea18349be2ab11f14c8b7c24
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-08-14 15:07:30 +0200

    Drop the game reference from game object

--- game/object/GameObject.ts
+++ game/object/GameObject.ts
@@ -1,13 +1,6 @@
 import Canvas2DRenderer from '../../engine/Canvas2DRenderer'
-import Game from '../../engine/Game.js'
 import TickUpdatedGameObject from '../../engine/TickUpdatedGameObject.js'
 import Pseudo3DCamera from '../Pseudo3DCamera.js'
 
 export default class GameObject extends TickUpdatedGameObject<Pseudo3DCamera, Canvas2DRenderer<Pseudo3DCamera>> {
-  constructor(
-    public readonly game: Game<Pseudo3DCamera, Canvas2DRenderer<Pseudo3DCamera>>,
-    x: number = 0, y: number = 0, z: number = 0,
-  ) {
-    super(x, y, z)
-  }
 }
