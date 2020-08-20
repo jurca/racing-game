4e816f1f7855cc3c3334c74f24af4aa191a8fe8d
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-08-20 18:51:37 +0200

    Lift the drawPolygon method to the IRenderer interface

--- engine/Renderer.ts
+++ engine/Renderer.ts
@@ -1,10 +1,16 @@
 import Camera from './Camera.js'
 import Game from './Game.js'
+import Point3D from './Point3D.js'
 
 export interface IRenderer<C extends Camera> {
   readonly camera: C
 
   render(game: Game<C, this>, deltaTime: number): void
+
+  drawPolygon(
+    color: string,
+    ...points: readonly [Readonly<Point3D>, Readonly<Point3D>, Readonly<Point3D>, ...ReadonlyArray<Readonly<Point3D>>]
+  ): void
 }
 
 export default abstract class Renderer<C extends Camera> implements IRenderer<C> {
@@ -18,4 +24,9 @@ export default abstract class Renderer<C extends Camera> implements IRenderer<C>
       gameObject.render(this, deltaTime)
     }
   }
+
+  public abstract drawPolygon(
+    color: string,
+    ...points: readonly [Readonly<Point3D>, Readonly<Point3D>, Readonly<Point3D>, ...ReadonlyArray<Readonly<Point3D>>]
+  ): void
 }
