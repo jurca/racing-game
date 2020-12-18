42fe0e375a1fedef6b448c607bd7ebbd765963ae
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-12-18 09:45:26 +0100

    Add lerp (linear interpolation) helpers

--- engine/Point3D.ts
+++ engine/Point3D.ts
@@ -1,4 +1,14 @@
+import {lerp} from './util.js'
+
 export default class Point3D {
+  public static lerp(start: Readonly<Point3D>, end: Readonly<Point3D>, progress: number): Point3D {
+    return new Point3D(
+      lerp(start.x, end.x, progress),
+      lerp(start.y, end.y, progress),
+      lerp(start.z, end.z, progress),
+    )
+  }
+
   constructor(
     public x: number = 0,
     public y: number = 0,
@@ -20,5 +30,9 @@ export default class Point3D {
       this.y - otherPoint.y,
       this.z - otherPoint.z,
     )
+  }
+
+  public lerpTo(end: Readonly<Point3D>, progress: number): Point3D {
+    return Point3D.lerp(this, end, progress)
   }
 }
--- engine/util.ts
+++ engine/util.ts
@@ -1,3 +1,8 @@
 export function lastItem<E>(items: readonly [E, ...E[]]): E {
   return items[items.length - 1]
 }
+
+export function lerp(start: number, end: number, progress: number): number {
+  return start * (1 - progress) + end * progress
+}
+
