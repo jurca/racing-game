f6ddf7a06a90483fff508c82f60acc467b3208c9
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-11-12 14:55:06 +0100

    Outline purpose of World and Track game objects

---
+++ game/object/Track.ts
@@ -0,0 +1,6 @@
+// Rendering:
+// 1. draw track segments - find the first visible track segment, render the
+// farthest track segment based on configured render distance and work the way
+// towards the first visible track segment.
+// 1.1. draw other NPCs at the segment
+// 1.2. draw fog after each segment
---
+++ game/object/World.ts
@@ -0,0 +1,4 @@
+// 1. Render sky box (well, sky cylinder really. update sky box position to fixed position relative to current camera
+// position to keep it at "infinity", some extra horizontal shifting might be preferable)
+// 2. Render Track (Track must render other cars)
+// 3. Render the player
