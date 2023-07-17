1084b3262f45bb4efe4ec37be7e646a2cdf9b40d
Martin Jurča <martin.jurca@firma.seznam.cz>
2023-07-17 19:13:14 +0200

    Update World GameObject plans

--- game/object/World.ts
+++ game/object/World.ts
@@ -1,4 +1,5 @@
 // 1. Render sky box (well, sky cylinder really. update sky box position to fixed position relative to current camera
-// position to keep it at "infinity", some extra horizontal shifting might be preferable)
-// 2. Render Track (Track must render other cars)
+//    position to keep it at "infinity", some extra horizontal shifting might be preferable)
+// 2. Render Track & NPC cars
 // 3. Render the player
+// 4. Render the HUD
